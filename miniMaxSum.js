8ccc441fa4e670caec0fff1a36816018743d6a66
Avelino Tiu III <thirdtiu@gmail.com>
2018-06-26 12:55:16 +0800

    solved miniMax problem

--- miniMaxSum.js
+++ miniMaxSum.js
@@ -1,5 +1,13 @@
+function sum (x, y) {
+    return Number(x) + Number(y);
+}
 function miniMaxSum(arr) {
-    
+    const sortedArray = arr.sort();
+    const min = sortedArray.slice(0, arr.length - 1).reduce(sum, 0);
+    const max = sortedArray.slice(1, arr.length).reduce(sum, 0);
+    console.log(min, max);
+
+    return(min + ' ' + max);
 }
 
 
